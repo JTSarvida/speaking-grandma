2cf8eeaa64cb743bd2585f216dbcd759ea39374e
jtsarvida <jtsarvida@gmail.com>
2022-06-30 02:35:35 -0400

    commit tree 8c5f5debcaf3ccb04d1796f0ad8fd05eb832463b

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,14 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(words)
+    if words != words.upcase
+        return "HUH?! SPEAK UP, SONNY!"
+    elsif words == "I LOVE YOU GRANDMA!"
+        return "I LOVE YOU TOO PUMPKIN!"
+    elsif words == words.upcase
+        return "NO, NOT SINCE 1938!"
+    end
+end
+
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
